403fb1fa626ab3f2105189e779faa6a6d3bc6a35
ssp13 <ssp13@users.noreply.github.com>
2015-04-08 22:25:03 +0300

    df

---
+++ server.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ssp on 29/3/2015.
+ */
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 3000);
